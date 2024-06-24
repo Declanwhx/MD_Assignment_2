15e2d3cc927a5ac3cea68d6affed09ddfcd74105
Declanwhx <declanwee@gmail.com>
2024-06-25 00:14:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import scipy.constants as co
-import re
 import lammps_logfile
 
 # # # # # # # # # # # # # # # CONSTANTS # # # # # # # # # # # # # # #
